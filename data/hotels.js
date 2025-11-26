09f814e5cbad8ad6f88b68f6635945644b9ad7d4
Lisette Jones <joneslis004@gmail.com>
2025-11-26 13:43:00 -0700

    commit too hotel

---
+++ data/hotels.js
@@ -0,0 +1,74 @@
+export const hotels = [
+  {
+    "image": "images/EmiratesPalace.jpg",
+    "name": "Emirates Palace",
+    "phone": "555-820-4411",
+    "address": [
+      "West Corniche Road",
+      "Abu Dhabi, UAE"
+    ]
+  },
+  {
+    "image": "images/bellagio.jpg",
+    "name": "The Bellagio",
+    "phone": "555-449-2290",
+    "address": [
+      "3600 Las Vegas Blvd S",
+      "Las Vegas, NV 89109"
+    ]
+  },
+  {
+    "image": "images/mgmgrand.jpg",
+    "name": "MGM Grand",
+    "phone": "555-772-9088",
+    "address": [
+      "3799 Las Vegas Blvd S",
+      "Las Vegas, NV 89109"
+    ]
+  },
+  {
+    "image": "images/beverlyhills.jpg",
+    "name": "Beverly Hills Hotel",
+    "phone": "555-662-4410",
+    "address": [
+      "9641 Sunset Boulevard",
+      "Beverly Hills, CA 90210"
+    ]
+  },
+  {
+    "image": "images/soneva.jpg",
+    "name": "Soneva Jani",
+    "phone": "555-339-8842",
+    "address": [
+      "Medhufaru Island",
+      "Noonu Atoll, Maldives"
+    ]
+  },
+  {
+    "image": "images/JumbyBay.jpg",
+    "name": "Jumby Bay",
+    "phone": "555-528-1127",
+    "address": [
+      "Jumby Bay Island",
+      "Antigua & Barbuda"
+    ]
+  },
+  {
+    "image": "images/marinabay.jpg",
+    "name": "Marina Bay Sands",
+    "phone": "555-204-7701",
+    "address": [
+      "10 Bayfront Avenue",
+      "Singapore 018956"
+    ]
+  },
+  {
+    "image": "images/royalcarl.jpg",
+    "name": "Royal Hotel Carlton",
+    "phone": "555-998-4510",
+    "address": [
+      "Via Montebello 8",
+      "Bologna, Italy 40121"
+    ]
+  }
+]
